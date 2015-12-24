f8b85a936d76e327ee71cf928805491abb293a72
zc1415926 <zc1415926@126.com>
2015-12-24 16:17:22 +0800

    using dropdown to show grade and class

--- resources/views/principle/index.blade.php
+++ resources/views/principle/index.blade.php
@@ -3,24 +3,73 @@
     <h1>把上课调皮的学生记到这里来！</h1>
 
     <script>
-        getGradeClass();
-        var gradeClassData;
+
+       getGradeClass();
+
+
         function getGradeClass()
         {
-            $.get('http://10.1.44.111/api/gradeclass', function(data, status){
+            $.ajax({
+                type: "get",
+                url: "http://10.1.44.111/api/grade",
+                success: function(data){
+                    data.forEach(function(e){
 
-                gradeClassData = JSON.stringify(data);
+                        $("#grade-ul").append(
+                                '<li><a href="#" onclick="onGradeDropdownClicked(' + e["Sgrade"] + ')">'
+                                + e["Sgrade"]
+                                + '年级</a></li>'
+                        );
+                    });
+                },
+                error: function(data){
+                    console.log("get an error when request gradeclassdata");
+                    console.log(data);
+                }
+            });
+        }
 
-                console.log("Status: " + status);
-                console.log("\nData:\n" +　gradeClassData);
-            })
-           // console.log($.toJSON(jsonData));
-            gradeClassData.forEach(function(e){
-                console.log(e);
-            })
+        function onGradeDropdownClicked(grade)
+        {
+            console.log(grade+"年级");
+            $.ajax({
+                type: "get",
+                url: "http://10.1.44.111/api/class/" + grade,
+                success: function(data){
 
-
-
+                    $("#class-ul li").remove();
+                    data.forEach(function(e){
+                        console.log(e["Sclass"]);
+                        $("#class-ul").append(
+                                '<li><a href="#"> ' + e["Sclass"] + '班</a></li>'
+                        );
+                    });
+                },
+                error: function(data){
+                    console.log("get an error when request gradeclassdata");
+                    console.log(data);
+                }
+            });
         }
     </script>
+
+    <div class="uk-button-dropdown" data-uk-dropdown="">
+        <button class="uk-button"> 请选择年级 <i class="uk-icon-caret-down"></i></button>
+        <div class="uk-dropdown">
+            <ul id="grade-ul" class="uk-nav uk-nav-dropdown">
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="uk-button-dropdown" data-uk-dropdown="">
+        <button class="uk-button"> 请选择班级 <i class="uk-icon-caret-down"></i></button>
+        <div class="uk-dropdown">
+            <ul id="class-ul" class="uk-nav uk-nav-dropdown">
+
+            </ul>
+        </div>
+    </div>
+
+
 @endsection
