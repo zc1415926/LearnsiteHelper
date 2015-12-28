14357753220dbd080e6f9782bb7fb56a96cdf462
zc1415926 <zc1415926@126.com>
2015-12-28 09:09:32 +0800

    you can get students list when select grade and class using dropdown button

--- resources/views/principle/index.blade.php
+++ resources/views/principle/index.blade.php
@@ -48,22 +48,22 @@
             });
         }
 
-        function onGradeDropdownClicked(grade)
+        function onGradeDropdownClicked(Sgrade)
         {
-            $("#grade-button").text(grade + "年级 ")
+            $("#grade-button").text(Sgrade + "年级 ")
                     .append('<span class="am-icon-caret-down"></span>');
             $("#class-button").text("请选择班级 ")
                     .append('<span class="am-icon-caret-down"></span>');
 
             $.ajax({
                 type: "get",
-                url: "http://10.1.44.111/api/class/" + grade,
+                url: "http://10.1.44.111/api/class/" + Sgrade,
                 success: function(data){
 
                     $("#class-ul li").remove();
                     data.forEach(function(e){
                         $("#class-ul").append(
-                                '<li><a href="#" onclick="onClassDropdownClicked(' + e["Sclass"] + ')"> ' + e["Sclass"] + '班</a></li>'
+                                '<li><a href="#" onclick="onClassDropdownClicked(' + Sgrade + ',' + e["Sclass"] + ')"> ' + e["Sclass"] + '班</a></li>'
                         );
                     });
                 },
@@ -74,10 +74,30 @@
             });
         }
 
-        function onClassDropdownClicked(Sclass)
+        function onClassDropdownClicked(Sgrade, Sclass)
         {
             $("#class-button").text(Sclass + "班 ")
                     .append('<span class="am-icon-caret-down"></span>');
+
+            $.ajax({
+                type: "get",
+                url: "http://10.1.44.111/api/students_gradeclass/" + Sgrade + "/" + Sclass,
+                success: function(data){
+
+                    $('#students-table-tbody tr').remove();
+
+                    data.forEach(function(e){
+
+                        $('#students-table-tbody').append('<tr><td>' + e["Snum"]
+                                + '</td><td>' + e["Sname"]
+                                + '</td><td>操作选项</td></tr>');
+                    });
+                },
+                error: function(data){
+                    console.log("get an error when request students");
+                    console.log(data);
+                }
+            });
         }
     </script>
 
@@ -102,34 +122,23 @@
                             </ul>
                         </div>
                     </div>
-
-                    <div class="am-u-lg-6 am-u-md-4 am-u-sm-12">
-                        <form class="am-form">
-                            <label for="name" class="about-color">你的姓名</label>
-                            <input id="name" type="text">
-                            <br>
-                            <label for="email" class="about-color">你的邮箱</label>
-                            <input id="email" type="email">
-                            <br>
-                            <label for="message" class="about-color">你的留言</label>
-                            <textarea id="message"></textarea>
-                            <br>
-                            <button type="submit" class="am-btn am-btn-primary am-btn-sm"><i class="am-icon-check"></i> 提 交</button>
-                        </form>
-                        <hr class="am-article-divider am-show-sm-only">
-                    </div>
-
-                    <div class="am-u-lg-6 am-u-md-8 am-u-sm-12">
-                        <h4 class="about-color">关于我们</h4>
-
-                        <p>AllMobilize Inc (美通云动科技有限公司)
-                            由前微软美国总部IE浏览器核心研发团队成员及移动互联网行业专家在美国西雅图创立，旨在解决网页在不同移动设备屏幕上的适配问题。基于国际专利技术并结合最前沿的HTML5技术，云适配解决方案可以帮助企业快速将桌面版网站适配到各种移动设备终端的屏幕上，不仅显著地提高了企业网站的用户体验以及销售转化率，而且大幅度地节省了企业开发和维护移动网站的费用。</p>
-                        <h4 class="about-color">团队介绍</h4>
-
-                        <p>AllMobilize Inc 获得了微软创投孵化器的支持，其领先科技已得到全球多家企业及机构的认可与信赖，客户包括全球500强企业、美国政府、国内政府机关、国内外上市公司、以及互联网标准化组织W3C。</p>
-                    </div>
                 </div>
+
+                <div class="students">
+                    <br>
+                    <table class="am-table am-table-striped am-table-hover about-color">
+                        <thead>
+                        <tr><th>学号</th>
+                            <th>姓名</th>
+                            <th>创建时间</th>
+                        </tr></thead>
+                        <tbody id="students-table-tbody">
+
+                        </tbody>
+                    </table></div>
             </div>
         </div>
     </div>
+
+
 @endsection
